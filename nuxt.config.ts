02b87f5f62fc9a079891c52bccd50f5349db7bd4
hsbmaulana <hsbmaulana.iso@gmail.com>
2024-01-10 13:40:27 +0700

    Initial Codebase : Configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,18 @@ export default defineNuxtConfig ({
         strict: true,
         typeCheck: true,
     },
+
+    // https://masteringnuxt.com/blog/configuration-in-nuxt-3-runtimeConfig-vs-appConfig
+    // https://nuxt.com/docs/api/composables/use-runtime-config
+
+    runtimeConfig: {
+
+        secret: process.env.SECRET,
+
+        public: {
+
+            appName: process.env.NUXT_PUBLIC_APP_NAME || "codebase",
+            env: process.env.NUXT_PUBLIC_APP_ENV || "production",
+        },
+    },
 });
