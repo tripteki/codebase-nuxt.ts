4e93bf0b17014f22bb535122416da10f41310d25
hsbmaulana <hsbmaulana.iso@protonmail.com>
2024-06-03 02:09:51 +0700

    Auth : Template

--- pages/admin/dashboard.vue
+++ pages/admin/dashboard.vue
@@ -1,3 +1,5 @@
 <template>
-    <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:hover:bg-blue-900 dark:text-blue-400">Test</button>
+    <div class="container m-2">
+        <button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:hover:bg-blue-900 dark:text-blue-400">Sign out</button>
+    </div>
 </template>
