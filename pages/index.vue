f85bb88fc8445b5aaba551387014cce68db89466
hsbmaulana <hsbmaulana.iso@protonmail.com>
2025-01-20 04:54:06 +0700

    feat: I18N

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 
-import { Avatar, AvatarFallback, AvatarImage, } from "@/components/ui/avatar";
-
 definePageMeta ({
 
     layout: "default",
@@ -15,10 +13,10 @@ useHead ({
 </script>
 
 <template>
-    <div class="underline">{{ $t ("common.welcome") }}</div>
-
-    <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback></AvatarFallback>
-    </Avatar>
+    <div class="mx-2">
+        <div>{{ $t ("common.welcome") }}</div>
+        <select v-model="$i18n.locale" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-25 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale.toUpperCase () }}</option>
+        </select>
+    </div>
 </template>
